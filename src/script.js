1b8c1a547e9be3bcbd24844a6c65c0bfc635ae40
TanCodeDeV <bhavetanu@gmail.com>
2025-01-17 12:22:20 +0530

    Added children routes for header elements

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import AboutUS from "./components/AboutUS";
 import ContactUS from "./components/ContactUS";
 import ErrorPage from "./components/ErrorPage";
@@ -12,25 +12,33 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header></Header>
-      <Body></Body>
+      <Outlet></Outlet>
       <Footer></Footer>
     </div>
   );
 };
 
+//this is client side routing, as react is a single page apppliaction, it just loads the resptive compinnenet when we click of link, it is not cfecthing data as server side scripting
+
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout></AppLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Body></Body>,
+      },
+      {
+        path: "/about",
+        element: <AboutUS></AboutUS>,
+      },
+      {
+        path: "/contact",
+        element: <ContactUS></ContactUS>,
+      },
+    ],
     errorElement: <ErrorPage></ErrorPage>,
-  },
-  {
-    path: "/about",
-    element: <AboutUS></AboutUS>,
-  },
-  {
-    path: "/contact",
-    element: <ContactUS></ContactUS>,
   },
 ]);
 
