ac846a473b3993cba76f3ab2c1d1cad0e6e15601
TanCodeDeV <bhavetanu@gmail.com>
2025-01-19 12:55:52 +0530

    Tried using class based component

--- src/components/ContactUS.js
+++ src/components/ContactUS.js
@@ -3,11 +3,11 @@ import UserClass from "./UserClass";
 const ContactUS = () => {
   return (
     <div>
-      <h1>Need a cool Website like this Contact me</h1>
+      <h1>Coooollllll!!😎</h1>
       <ul>
         <li>Phone no: +91-9405889363</li>
         <li>Email: tanMe@gmail.com</li>
-        <User name={"Tanmayi Bhave"} add={"Pune"} email={"TanR@gmail.com"} />
+        <User name={"Tanmayi Bhave"} add={"Pune"} email={"tanmayi@gmail.com"} />
 
         <UserClass name={"Joker"} add={"Pune"} email={"joker@234"} />
       </ul>
